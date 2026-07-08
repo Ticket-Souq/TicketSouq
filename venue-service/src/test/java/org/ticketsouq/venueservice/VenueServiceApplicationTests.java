bb2750bc8c33ca4e23bec21ef024ddeec3b58afd
Ahmed Habib <ahmedhabib0660@gmail.com>
2026-07-08 20:41:55 +0300

    init venue service

---
+++ venue-service/src/test/java/org/ticketsouq/venueservice/VenueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ticketsouq.venueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
