bb7d1b5122e09bbbcffb6351e14091473cd0083d
omar <202027471@std.sci.cu.edu.eg>
2026-07-09 23:34:21 +0300

    add: EmailVerificationConsumer

---
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/consumer/EmailVerificationConsumer.java
@@ -0,0 +1,21 @@
+package org.ticketsouq.notificationservice.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.ticketsouq.notificationservice.event.EmailVerificationEvent;
+import org.ticketsouq.notificationservice.service.NotificationService;
+
+@Component
+public class EmailVerificationConsumer {
+    private final NotificationService notificationService;
+
+    public EmailVerificationConsumer(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @KafkaListener(topics = "email-verification")
+    public void consume(EmailVerificationEvent event) {
+        notificationService.handleEmailVerification(event);
+    }
+
+}
