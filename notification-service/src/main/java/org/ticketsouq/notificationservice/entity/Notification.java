06cfe7da66fc6d284a229984014939ed7ea3248b
omar <202027471@std.sci.cu.edu.eg>
2026-07-09 19:32:47 +0300

    add Notification and NotificationTypes

---
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/entity/Notification.java
@@ -0,0 +1,54 @@
+package org.ticketsouq.notificationservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.ticketsouq.notificationservice.enums.NotificationType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "notifications",
+    indexes = {
+        @Index(name = "idx_user_read", columnList = "user_id,is_read")
+    }
+)
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "event_id")
+    private Long eventId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationType type;
+
+    @Column(name = "is_read", nullable = false)
+    @Builder.Default
+    private boolean isRead = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public void markAsRead() {
+        this.isRead = true;
+    }
+
+}
---
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/enums/NotificationType.java
@@ -0,0 +1,13 @@
+package org.ticketsouq.notificationservice.enums;
+
+public enum NotificationType {
+    REGISTRATION,
+
+    PASSWORD_RESET,
+
+    PASSWORD_CHANGED,
+
+    PAYMENT_SUCCESS,
+
+    EVENT_CANCELLED
+}
