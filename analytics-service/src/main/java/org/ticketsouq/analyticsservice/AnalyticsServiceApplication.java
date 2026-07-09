06c4b94bce89b26618ec465d2fb16e9262d5a313
Ahmed <ahmadazeem40@gmail.com>
2026-07-09 14:32:30 +0300

    feat: add hello world(test pr)

--- analytics-service/src/main/java/org/ticketsouq/analyticsservice/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/org/ticketsouq/analyticsservice/AnalyticsServiceApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnalyticsServiceApplication {
-
+    // Ahmad Mohammed Abdulazeem comment
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsServiceApplication.class, args);
+        System.out.println("hello world");
     }
 
 }
