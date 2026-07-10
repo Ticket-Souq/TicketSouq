47928c5e313a45e83319f5db6a4cf6b7a8852863
omar <202027471@std.sci.cu.edu.eg>
2026-07-10 03:33:48 +0300

    add: mock send Verification Email

--- notification-service/src/main/java/org/ticketsouq/notificationservice/service/impl/EmailServiceImpl.java
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/service/impl/EmailServiceImpl.java
@@ -1,18 +1,76 @@
 package org.ticketsouq.notificationservice.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.ticketsouq.notificationservice.event.EmailVerificationEvent;
 import org.ticketsouq.notificationservice.service.EmailService;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.TemplateEngine;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.context.Context;
 
 @Service
 public class EmailServiceImpl implements EmailService {
+    private static final Logger log =
+        LoggerFactory.getLogger(EmailServiceImpl.class);
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+
+    public EmailServiceImpl(JavaMailSender mailSender,
+                            TemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
 
     @Override
     public void sendVerificationEmail(EmailVerificationEvent event) {
-        System.out.println("================================");
-        System.out.println("Sending verification email");
-        System.out.println("User: " + event.email());
-        System.out.println("Token: " + event.token());
-        System.out.println("================================");
+        log.info("Step 1");
+
+        Context context = new Context();
+
+        log.info("Step 2");
+
+        String verificationUrl = "...";
+
+        context.setVariable("verificationUrl", verificationUrl);
+
+        log.info("Step 3");
+
+        String html = templateEngine.process("email/registration", context);
+
+        log.info("Step 4");
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        log.info("Step 5");
+
+        try {
+
+            log.info("Step 6");
+
+            MimeMessageHelper helper =
+                new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+            helper.setTo(event.email());
+
+            log.info("Step 7");
+
+            helper.setSubject("Verify your Ticketaty account");
+
+            helper.setText(html, true);
+
+            log.info("Step 8");
+
+            mailSender.send(mimeMessage);
+
+            log.info("Email Sent Successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
