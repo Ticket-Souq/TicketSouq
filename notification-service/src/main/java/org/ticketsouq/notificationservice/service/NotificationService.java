a2e89f996421345f97d57705b966e35e09057237
omar <202027471@std.sci.cu.edu.eg>
2026-07-10 08:33:25 +0300

    update NotificationService

--- notification-service/src/main/java/org/ticketsouq/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/service/NotificationService.java
@@ -1,9 +1,15 @@
 package org.ticketsouq.notificationservice.service;
 
+import org.ticketsouq.notificationservice.dto.NotificationResponse;
+import org.ticketsouq.notificationservice.dto.UnreadCountResponse;
+import org.ticketsouq.notificationservice.entity.Notification;
 import org.ticketsouq.notificationservice.event.EmailVerificationEvent;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface NotificationService {
 
     void handleEmailVerification(EmailVerificationEvent event);
-
-}
+    List<NotificationResponse> getNotifications(UUID userId);
+    UnreadCountResponse getUnreadCount(UUID userId);}
