9fc3e35d3b9b6da6cbdf85da30ec54154d299d03
omar <202027471@std.sci.cu.edu.eg>
2026-07-10 03:35:12 +0300

    update: handle Email Verification

--- notification-service/src/main/java/org/ticketsouq/notificationservice/service/impl/NotificationServiceImpl.java
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/service/impl/NotificationServiceImpl.java
@@ -6,25 +6,33 @@ import org.ticketsouq.notificationservice.event.EmailVerificationEvent;
 import org.ticketsouq.notificationservice.repository.UserEmailProjectionRepository;
 import org.ticketsouq.notificationservice.service.EmailService;
 import org.ticketsouq.notificationservice.service.NotificationService;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
     private final UserEmailProjectionRepository userEmailProjectionRepository;
     private final EmailService emailService;
+    private static final Logger log =
+        LoggerFactory.getLogger(NotificationServiceImpl.class);
+
     public NotificationServiceImpl(UserEmailProjectionRepository userEmailProjectionRepository, EmailService emailService) {
         this.userEmailProjectionRepository = userEmailProjectionRepository;
         this.emailService = emailService;
     }
+
     @Override
     public void handleEmailVerification(EmailVerificationEvent event) {
-
-        UserEmailProjection projection = new UserEmailProjection();
-        projection.setUserId(event.userId());
-        projection.setEmail(event.email());
-
-        userEmailProjectionRepository.save(projection);
+        if (!userEmailProjectionRepository.existsById(event.userId())) {
+            UserEmailProjection projection = UserEmailProjection.builder()
+                .userId(event.userId())
+                .email(event.email())
+                .build();
+            userEmailProjectionRepository.save(projection);
+            log.info("Email projection saved for user {}", event.userId());
+        }
+        log.info("Email projection saved for user {}", event.userId());
         emailService.sendVerificationEmail(event);
-
+        log.info("Verification email sent to {}", event.email());
     }
 }
