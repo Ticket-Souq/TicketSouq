1f6d784829f39407fce7e9a9cecd9b105051b168
omar <202027471@std.sci.cu.edu.eg>
2026-07-10 03:35:43 +0300

    bug: solve PK at UserEmailProjectionRepository

--- notification-service/src/main/java/org/ticketsouq/notificationservice/repository/UserEmailProjectionRepository.java
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/repository/UserEmailProjectionRepository.java
@@ -3,5 +3,7 @@ package org.ticketsouq.notificationservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ticketsouq.notificationservice.entity.UserEmailProjection;
 
-public interface UserEmailProjectionRepository extends JpaRepository<UserEmailProjection,Long> {
+import java.util.UUID;
+
+public interface UserEmailProjectionRepository extends JpaRepository<UserEmailProjection, UUID> {
 }
