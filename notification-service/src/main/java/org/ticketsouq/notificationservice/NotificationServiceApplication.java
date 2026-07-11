fdb0b2077b7c28ffdf059958d3974eb36a30e1c5
omar <202027471@std.sci.cu.edu.eg>
2026-07-11 06:25:15 +0300

    enable caching

--- notification-service/src/main/java/org/ticketsouq/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,13 @@ package org.ticketsouq.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
+@EnableFeignClients
+
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
