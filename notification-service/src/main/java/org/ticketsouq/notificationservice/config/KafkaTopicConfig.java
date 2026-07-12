1fc73b41cb621440f88d03150338be0409f94cc1
omar <202027471@std.sci.cu.edu.eg>
2026-07-12 04:32:40 +0300

    complete kafka topics config

--- notification-service/src/main/java/org/ticketsouq/notificationservice/config/KafkaTopicConfig.java
+++ notification-service/src/main/java/org/ticketsouq/notificationservice/config/KafkaTopicConfig.java
@@ -29,4 +29,26 @@ public class KafkaTopicConfig {
             .replicas(1)
             .build();
     }
+    @Bean
+    public NewTopic refundCompletedTopic() {
+        return TopicBuilder.name("notification.refund-completed")
+            .partitions(6)
+            .replicas(1)
+            .build();
+    }
+    @Bean
+    public NewTopic AccountGeneratedTopic() {
+        return TopicBuilder.name("notification.account-generated")
+            .partitions(6)
+            .replicas(1)
+            .build();
+    }
+    @Bean
+    public NewTopic PaymentSuccessTopic() {
+        return TopicBuilder.name("notification.payment-success")
+            .partitions(6)
+            .replicas(1)
+            .build();
+    }
+
 }
