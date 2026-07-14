052c010b47050cc8174b492958f7b0a4258f3c9d
omar <202027471@std.sci.cu.edu.eg>
2026-07-14 21:29:25 +0300

    hotfix: enforce zone name uniqueness in every event

--- event-service/src/main/java/org/ticketsouq/eventservice/repository/SectionRepository.java
+++ event-service/src/main/java/org/ticketsouq/eventservice/repository/SectionRepository.java
@@ -3,9 +3,8 @@ package org.ticketsouq.eventservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ticketsouq.eventservice.model.Section;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface SectionRepository extends JpaRepository<Section, UUID> {
-
+    boolean existsByEventIdAndName(UUID eventId, String name);
 }
