89b69b71190ea295e853c58f59ba25f924a336d3
omar <202027471@std.sci.cu.edu.eg>
2026-07-18 10:49:40 +0300

    EnableJpaAuditing

--- event-service/src/main/java/org/ticketsouq/eventservice/EventServiceApplication.java
+++ event-service/src/main/java/org/ticketsouq/eventservice/EventServiceApplication.java
@@ -3,12 +3,10 @@ package org.ticketsouq.eventservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "org.ticketsouq")
-@EnableJpaAuditing
 @EnableScheduling
 @EnableKafka
 @EnableFeignClients
