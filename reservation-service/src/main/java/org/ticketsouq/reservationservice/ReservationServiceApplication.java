23a0d506ef5ba7e31032623bf512180a53f9ee12
Ahmed Habib <ahmedhabib0660@gmail.com>
2026-07-20 20:57:02 +0300

    init reservation service

--- reservation-service/src/main/java/org/ticketsouq/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/org/ticketsouq/reservationservice/ReservationServiceApplication.java
@@ -2,8 +2,16 @@ package org.ticketsouq.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.ticketsouq")
+@EnableKafka
+@EnableFeignClients
+@EnableJpaAuditing
+@EnableScheduling
 public class ReservationServiceApplication {
 
     public static void main(String[] args) {
