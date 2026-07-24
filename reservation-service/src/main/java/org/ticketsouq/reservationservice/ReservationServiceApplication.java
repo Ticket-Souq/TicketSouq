a7a2a881a1d278db4b79b9834f1efd28f3b49e89
Ahmed Habib <ahmedhabib0660@gmail.com>
2026-07-24 19:20:07 +0300

    service ready to be tested

--- reservation-service/src/main/java/org/ticketsouq/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/org/ticketsouq/reservationservice/ReservationServiceApplication.java
@@ -2,16 +2,14 @@ package org.ticketsouq.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "org.ticketsouq")
 @EnableKafka
-@EnableFeignClients
-@EnableJpaAuditing
 @EnableScheduling
+@EnableJpaAuditing
 public class ReservationServiceApplication {
 
     public static void main(String[] args) {
