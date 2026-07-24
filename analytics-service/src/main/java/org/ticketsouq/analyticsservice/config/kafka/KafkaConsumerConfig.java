6e10d08423900a159e38966e4b98d25a83a89624
Ahmed <ahmadazeem40@gmail.com>
2026-07-24 16:54:40 +0300

    fix: kafka configs

--- analytics-service/src/main/java/org/ticketsouq/analyticsservice/config/kafka/KafkaConsumerConfig.java
+++ analytics-service/src/main/java/org/ticketsouq/analyticsservice/config/kafka/KafkaConsumerConfig.java
@@ -1,37 +1,22 @@
 package org.ticketsouq.analyticsservice.config.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.DataAccessException;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConsumerRecordRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.DeserializationException;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
-
-    @Bean
-    public ProducerFactory<String, Object> producerFactory() {
-        Map<String, Object> config = Map.of(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092",
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
-            JsonSerializer.ADD_TYPE_INFO_HEADERS, true
-        );
-        return new DefaultKafkaProducerFactory<>(config);
-    }
 
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
