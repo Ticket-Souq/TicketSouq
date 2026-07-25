4554cb5854d45b5b70944aca525b8a0d4dbdde9c
Ahmed Habib <ahmedhabib0660@gmail.com>
2026-07-26 01:24:54 +0300

    making ratelimit with redis

--- api-gateway/src/main/java/org/ticketsouq/apigateway/config/RedisConfig.java
+++ api-gateway/src/main/java/org/ticketsouq/apigateway/config/RedisConfig.java
@@ -1,10 +1,18 @@
 package org.ticketsouq.apigateway.config;
 
 
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -17,6 +25,20 @@ public class RedisConfig {
         template.setConnectionFactory(factory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public ProxyManager<String> proxyManager(RedisConnectionFactory connectionFactory) {
+        LettuceConnectionFactory lettuceFactory = (LettuceConnectionFactory) connectionFactory;
+        RedisClient redisClient = (RedisClient) lettuceFactory.getRequiredNativeClient();
+
+        StatefulRedisConnection<String, byte[]> connection = redisClient.connect(
+            RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE)
+        );
+
+        return LettuceBasedProxyManager.builderFor(connection).build();
     }
 }
