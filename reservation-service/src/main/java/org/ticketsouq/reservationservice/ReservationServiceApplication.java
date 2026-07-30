a43512a0c52fdcc062e187911b7c3881929c4c0e
omar <omar.sseeddeekk@gmail.com>
2026-07-31 02:40:45 +0300

    add ticketsouq-outbox dependency in ReservationService

--- reservation-service/src/main/java/org/ticketsouq/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/org/ticketsouq/reservationservice/ReservationServiceApplication.java
@@ -1,12 +1,14 @@
 package org.ticketsouq.reservationservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "org.ticketsouq")
+@AutoConfigurationPackage(basePackages = "org.ticketsouq")
 @EnableKafka
 @EnableScheduling
 @EnableJpaAuditing
