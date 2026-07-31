f26f2a7d18a5b6c8c338fa10a621b404c5abbb69
omar <omar.sseeddeekk@gmail.com>
2026-07-31 04:29:44 +0300

    change eventPublisher with outboxWriter in EventEventConsumer

--- event-service/src/main/java/org/ticketsouq/eventservice/event/EventEventConsumer.java
+++ event-service/src/main/java/org/ticketsouq/eventservice/event/EventEventConsumer.java
@@ -1,11 +1,11 @@
 package org.ticketsouq.eventservice.event;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.ticketsouq.eventservice.service.LockService;
+import org.ticketsouq.outbox.service.OutboxWriter;
 import org.ticketsouq.sharedmodule.EventService.exception.LockExpiredException;
 import org.ticketsouq.sharedmodule.EventService.exception.SeatAlreadyBookedException;
 import org.ticketsouq.sharedmodule.ReservationService.events.SagaLockConfirmCommand;
@@ -16,13 +16,14 @@ import org.ticketsouq.sharedmodule.utils.LogUtils;
 import static org.ticketsouq.sharedmodule.Constants.SERVICE_NAMES.EVENT_SERVICE;
 import static org.ticketsouq.sharedmodule.Constants.TOPIC_NAMES.SAGA_LOCK_CONFIRM_COMMAND;
 import static org.ticketsouq.sharedmodule.Constants.TOPIC_NAMES.SAGA_LOCK_CONFIRM_COMPENSATE;
+import static org.ticketsouq.sharedmodule.Constants.TOPIC_NAMES.SAGA_LOCK_CONFIRM_REPLY;
 
 @Component
 @RequiredArgsConstructor
 public class EventEventConsumer {
 
     private final LockService lockService;
-    private final ApplicationEventPublisher applicationEventPublisher;
+    private final OutboxWriter outboxWriter;
 
     @KafkaListener(topics = SAGA_LOCK_CONFIRM_COMMAND)
     @Transactional
@@ -30,9 +31,9 @@ public class EventEventConsumer {
         LogUtils.logEventConsumed(EVENT_SERVICE, SAGA_LOCK_CONFIRM_COMMAND);
         try {
             lockService.confirm(String.valueOf(command.reservationId()));
-            applicationEventPublisher.publishEvent(new SagaLockConfirmReplyEvent(command.reservationId(), true, ""));
+            outboxWriter.save(new SagaLockConfirmReplyEvent(command.reservationId(), true, ""), SAGA_LOCK_CONFIRM_REPLY, command.reservationId().toString());
         } catch (LockExpiredException | SeatAlreadyBookedException ex) {
-            applicationEventPublisher.publishEvent(new SagaLockConfirmReplyEvent(command.reservationId(), false, ex.getMessage()));
+            outboxWriter.save(new SagaLockConfirmReplyEvent(command.reservationId(), false, ex.getMessage()), SAGA_LOCK_CONFIRM_REPLY, command.reservationId().toString());
         }
     }
 
