13626c16f365e26be0b5cc6211623ad5a85413e1
omar <omar.sseeddeekk@gmail.com>
2026-07-31 06:51:22 +0300

    update payment service with the shared outbox

--- payment-service/src/main/java/org/ticketsouq/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/org/ticketsouq/paymentservice/PaymentServiceApplication.java
@@ -1,11 +1,13 @@
 package org.ticketsouq.paymentservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = "org.ticketsouq")
+@AutoConfigurationPackage(basePackages = "org.ticketsouq")
 @EnableKafka
 @EnableJpaAuditing
 public class PaymentServiceApplication {
