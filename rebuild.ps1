2a8d374c9a7cdbc4dd50f0f7911aab3d3497eec8
Ahmed Habib <ahmedhabib0660@gmail.com>
2026-08-07 22:56:28 +0300

    allow grafana to appear when use cloudflare

--- rebuild.ps1
+++ rebuild.ps1
@@ -13,6 +13,15 @@ Write-Host "========================================"
 
 docker compose up --build -d
 
+
+Write-Host ""
+Write-Host "========================================"
+Write-Host "Opening tunnel to your local host... (don't uncomment of you don't have cloudflare) "
+Write-Host "========================================"
+
+
+#cloudflared tunnel --url http://localhost:5173
+
 Write-Host ""
 Write-Host "========================================"
 Write-Host "Done!"
