a0e565e45bd6f9563cd68e8b6faa211daf91b2e4
Ahmed Habib <ahmedhabib0660@gmail.com>
2026-08-09 17:03:42 +0300

    add health check for all containers and update the rebuilds scripts

--- rebuild.ps1
+++ rebuild.ps1
@@ -4,21 +4,33 @@ Write-Host "========================================"
 Write-Host "Building Maven project..."
 Write-Host "========================================"
 
-mvn clean package -DskipTests
+# mvn clean package -DskipTests
 
 Write-Host ""
 Write-Host "========================================"
-Write-Host "Rebuilding Docker images and starting containers..."
+Write-Host "Remove Old images without Database volumes..."
 Write-Host "========================================"
 
-docker compose up --build -d
+docker compose down --remove-orphans --rmi local
 
+Write-Host ""
+Write-Host "========================================"
+Write-Host "Rebuilding Docker images..."
+Write-Host "========================================"
+
+docker compose build --no-cache
+
+Write-Host ""
+Write-Host "========================================"
+Write-Host "starting containers..."
+Write-Host "========================================"
+
+docker compose up -d
 
 Write-Host ""
 Write-Host "========================================"
 Write-Host "Opening tunnel to your local host... (don't uncomment of you don't have cloudflare) "
 Write-Host "========================================"
-
 
 #cloudflared tunnel --url http://localhost:5173
 
